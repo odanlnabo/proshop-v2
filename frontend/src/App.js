ffe32de46c22ccf21a0fcae38c0ae615401049df
Zarith Sofian <zarith@fratelli.com.my>
2023-06-15 15:53:27 +0800

    React Router Dom installed. Fix all link with React Router Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -10,8 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          {/* <h1>Welcome to ProShop</h1> */}
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
